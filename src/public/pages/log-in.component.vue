4c1ee31caf145c38ecf9980b937aec2ea6ee36eb
P1er0VL <112918737+P1er0VL@users.noreply.github.com>
2024-05-03 15:23:50 -0500

    Update log-in.component.vue

--- src/public/pages/log-in.component.vue
+++ src/public/pages/log-in.component.vue
@@ -13,7 +13,7 @@ export default {
   },
   methods:{
     async login(){
-      let result = await axios.get(`http://localhost:3000/users?email=${this.email}&password=${this.password}`)
+      let result = await axios.get(`https://my-json-server.typicode.com/G3-AuditBridge/AuditBridge-FakeAPI/users?email=${this.email}&password=${this.password}`)
       if(result.status==200 && result.data.length > 0){
         alert("Log In Successfully")
         localStorage.setItem("user-info",JSON.stringify(result.data[0]))
